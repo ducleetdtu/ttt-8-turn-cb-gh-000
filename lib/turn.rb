1adc2342353d64c90ededcc79f06028e950c6f27
Duc Le Dang <51703059@student.tdtu.edu.vn>
2020-02-01 05:56:58 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,3 +1,8 @@
-def display_board(board)
-  
+def display_board(state=[" "," "," "," "," "," "," "," "," "])
+  puts " #{state[0]} | #{state[1]} | #{state[2]} "
+  puts "-----------"
+  puts " #{state[3]} | #{state[4]} | #{state[5]} "
+  puts "-----------"
+  puts " #{state[6]} | #{state[7]} | #{state[8]} "
+
 end
